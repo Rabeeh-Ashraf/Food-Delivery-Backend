d92fd8aa1b35077f3658b8b3458596f87f9e8583
Rabeeh-Ashraf <rabeeh.ashraf2002@gmail.com>
2025-04-17 20:54:32 +0530

    Implement user registration with bcrypt hashing, JWT token, and strong password validation

--- Controllers/Usercontroller.js
+++ Controllers/Usercontroller.js
@@ -1,17 +1,58 @@
-import userModel from '../Models/Usermodel.js';
-import jwt from 'jsonwebtoken'
-import bcrypt from "bcrypt"
-import validator from "validator"
+import userModel from "../Models/Usermodel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
 
 //Login user
-const loginUser = async (req,res)=>{
-
-}
-
+const loginUser = async (req, res) => {};
 
 //Register User
-const registerUser =async (req,res)=>{
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
 
-}
-
-export {loginUser,registerUser}
+  try {
+    //check if all fields are provided
+    const exists = await userModel.findOne({ email });
+    if (!name || !email || !password) {
+      return res.json({ success: false, message: "All fields are required" });
+    }
+    //check if user already exist
+    if (exists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    //validating email & strong password
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "Please enter a valid email",
+      });
+    }
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+    //hashing the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    //create an new user to database
+    const newuser = new userModel({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+    const user = await newuser.save();
+    //generate jwd token
+    const createtoken = (id) => {
+      return jwt.sign({ id }, process.env.JWT_SECRET);
+    };
+    //send signup succcess
+    const token = createtoken(user._id);
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+export { loginUser, registerUser };
