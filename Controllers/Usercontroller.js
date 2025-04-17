90fbeba6408dcc118528815c6c97441551593002
Rabeeh-Ashraf <rabeeh.ashraf2002@gmail.com>
2025-04-17 19:47:13 +0530

    Add User controller with login and register functions, connect to router and server

---
+++ Controllers/Usercontroller.js
@@ -0,0 +1,17 @@
+import userModel from '../Models/Usermodel.js';
+import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt"
+import validator from "validator"
+
+//Login user
+const loginUser = async (req,res)=>{
+
+}
+
+
+//Register User
+const registerUser =async (req,res)=>{
+
+}
+
+export {loginUser,registerUser}
--- Server.js
+++ Server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { ConnectDb } from "./Config/Db.js"
 import foodRouter from "./Routes/Foodroute.js"
+import userRouter from "./Routes/Userroute.js"
 
 
 
@@ -18,6 +19,7 @@ ConnectDb()
 //API End Point
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('Uploads'))
+app.use("/api/user",userRouter)
 
  app.get("/",(req,res)=>{
         res.send("API Working")
