9dcf83c82d02309fca5c023b977ec270350a455d
Rabeeh-Ashraf <rabeeh.ashraf2002@gmail.com>
2025-04-22 16:17:07 +0530

    Added Cart routes with JWT AuthMiddleware for add, remove, and get cart operations

--- Routes/Cartroute.js
+++ Routes/Cartroute.js
@@ -1,12 +1,13 @@
 import express from "express"
 import { addToCart,removeFromcart,getCart } from "../Controllers/Cartcontroller.js"
+import AuthMiddleware from "../Middleware/Auth.js";
 
 
 const Cartrouter = express.Router();
 
-Cartrouter.post("/add",addToCart);
-Cartrouter.post("/remove",removeFromcart);
-Cartrouter.post("/get",getCart);
+Cartrouter.post("/add",addToCart,AuthMiddleware);
+Cartrouter.post("/remove",removeFromcart,AuthMiddleware);
+Cartrouter.post("/get",getCart,AuthMiddleware);
 
 export default Cartrouter 
 
