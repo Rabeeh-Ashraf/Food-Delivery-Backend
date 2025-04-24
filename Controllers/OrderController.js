01da10c1576b77b52328b2a1e949df00714f64ac
Rabeeh-Ashraf <rabeeh.ashraf2002@gmail.com>
2025-04-24 21:29:18 +0530

    Added logic to create and save new order to database And Cleared user cart after successful order And Prepared Stripe-compatible line items including delivery charges

--- Controllers/OrderController.js
+++ Controllers/OrderController.js
@@ -1,3 +1,4 @@
+import { CurrencyCodes } from "validator/lib/isISO4217.js";
 import orderModel from "../Models/OrderModel.js";
 import userModel from "../Models/Usermodel.js"
 import Stripe from "stripe"
@@ -6,6 +7,44 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //placing user model from fronted
 const placeOrder = async (req,res)=>{
-     
+
+    const frontend_url = "http://localhost:3000"
+
+     try {
+        const newOrder = new orderModel({
+            userId:req.body.userId,
+            items : req.body.items,
+            amount : req.body.amount,
+            address: req.body.address
+        })
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}});
+
+        const line_items = req.body.items.map((item)=>({
+            price_data :{
+                currency :"inr",
+                product_data:{
+                    name:item.name
+                },
+                unit_amount:item.price*100*80
+            },
+            quantity : item.quantity 
+        }))
+
+            line_items.push({
+                price_data : {
+                    currency :"inr",
+                    product_data:{
+                        name:"Delivery Charges"
+                    },
+                    unit_amount:2*100*80
+                },
+                quantity:1
+            })
+
+            
+     } catch (error) {
+        
+     }
 }
 export {placeOrder}
